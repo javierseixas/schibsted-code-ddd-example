cf52a08a87a14854c8a6525f647fe7812446c432
Kevin Mas Ruiz <masruizkevin@gmail.com>
2016-02-29 21:59:15 +0100

    accepts pending posts

--- src/main/kotlin/kmruiz/domain/post/Post.kt
+++ src/main/kotlin/kmruiz/domain/post/Post.kt
@@ -1,7 +1,20 @@
 package kmruiz.domain.post
 
+import java.util.*
+
 interface PostPublisher {
     fun publishPost(title: String, body: String): Post
 }
 
-data class Post(val author: Long, val authorName: String, val title: String, val body: String)
+interface Post
+interface PendingPost: Post
+interface PublishedPost: Post
+
+data class SinglePendingPost(val author: Long, val authorName: String, val title: String, val body: String): PendingPost
+data class SinglePublishedPost(
+        val author: Long,
+        val authorName: String,
+        val title: String,
+        val body: String,
+        val publicationDate: Date = Date()
+): PublishedPost
--- src/main/kotlin/kmruiz/domain/user/User.kt
+++ src/main/kotlin/kmruiz/domain/user/User.kt
@@ -1,11 +1,16 @@
 package kmruiz.domain.user
 
-import kmruiz.domain.post.Post
 import kmruiz.domain.post.PostPublisher
+import kmruiz.domain.post.SinglePendingPost
+import kmruiz.domain.post.SinglePublishedPost
 
-interface User
-interface Administrator: User, PostPublisher
+interface User: PostPublisher
+interface Administrator: User
+
+data class BlogUser(val id: Long, val name: String): User {
+    override fun publishPost(title: String, body: String) = SinglePendingPost(id, name, title, body)
+}
 
 data class BlogAdministrator(val id: Long, val name: String): Administrator {
-    override fun publishPost(title: String, body: String) = Post(id, name, title, body)
+    override fun publishPost(title: String, body: String) = SinglePublishedPost(id, name, title, body)
 }
