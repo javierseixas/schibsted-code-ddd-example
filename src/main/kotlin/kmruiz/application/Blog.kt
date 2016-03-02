2b126931288974c8ec6f47551d3574d68305f297
Kevin Mas Ruiz <masruizkevin@gmail.com>
2016-03-02 22:12:45 +0100

    lastPosts on app service

--- src/main/kotlin/kmruiz/application/Blog.kt
+++ src/main/kotlin/kmruiz/application/Blog.kt
@@ -2,11 +2,18 @@ package kmruiz.application
 
 import kmruiz.domain.post.Post
 import kmruiz.domain.post.PostService
+import kmruiz.domain.post.PostView
 import kmruiz.domain.user.UserService
+import java.util.*
 
 data class Blog(val userService: UserService, val postService: PostService) {
     fun publishPost(userId: Long, title: String, body: String): Post {
         val user = userService.findUser(userId)
         return postService.publish(user, title, body)
+    }
+
+    fun lastPosts(userId: Long, startDate: Date): List<PostView> {
+        val user = userService.findUser(userId)
+        return postService.last(user, startDate)
     }
 }
